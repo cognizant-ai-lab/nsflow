438ccbc13e861f8e98c2474cd1e1f78410fdbc1e
deepsaia <deepak.singh10@cognizant.com>
2025-03-14 19:10:45 -0700

    pylint

--- nsflow/backend/api/v1/version_info.py
+++ nsflow/backend/api/v1/version_info.py
@@ -1,6 +1,6 @@
+import logging
 from fastapi import APIRouter
 import pkg_resources
-import logging
 
 router = APIRouter(prefix="/api/v1")
 
@@ -8,17 +8,17 @@ router = APIRouter(prefix="/api/v1")
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def get_version(package_name: str):
     """Get the version from installed package"""
     try:
         # Fetch version from installed package
         return pkg_resources.get_distribution(package_name).version
     except pkg_resources.DistributionNotFound as e:
-        logger.error(f"Package '{package_name}' not found: {e}")
+        logger.error("Package '%s' not found: %s", package_name, e)
         if package_name == "nsflow":
-            return "dev.0.5.0"
-        else:
-            return "not found"
+            return "dev.version"
+        return "not found"
 
 
 @router.get("/version/{package_name}")
