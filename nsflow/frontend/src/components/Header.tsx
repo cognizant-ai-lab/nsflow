472d857a452d0d0c081d7a92411fca268c1a7f10
Singh <228496@us-k7qqqjr3cx-cts.attlocal.net>
2025-04-10 14:15:33 -0700

    formatted copyright comments for relevant file types

--- nsflow/frontend/src/components/Header.tsx
+++ nsflow/frontend/src/components/Header.tsx
@@ -1,14 +1,14 @@
 
-# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
-# All Rights Reserved.
-# Issued under the Academic Public License.
-#
-# You can be released from the terms, and requirements of the Academic Public
-# License by purchasing a commercial license.
-# Purchase of a commercial license is mandatory for any use of the
-# ENN-release SDK Software in commercial settings.
-#
-# END COPYRIGHT
+// Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
+// All Rights Reserved.
+// Issued under the Academic Public License.
+
+// You can be released from the terms, and requirements of the Academic Public
+// License by purchasing a commercial license.
+// Purchase of a commercial license is mandatory for any use of the
+// ENN-release SDK Software in commercial settings.
+
+// END COPYRIGHT
 import React, { useState, useRef, useEffect } from "react";
 import { FaUserCircle, FaFolderOpen, FaSave, FaDownload } from "react-icons/fa";
 import { ImPower  } from "react-icons/im";
--- nsflow/frontend/src/components/InfoPanel.tsx
+++ nsflow/frontend/src/components/InfoPanel.tsx
@@ -1,14 +1,14 @@
 
-# Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
-# All Rights Reserved.
-# Issued under the Academic Public License.
-#
-# You can be released from the terms, and requirements of the Academic Public
-# License by purchasing a commercial license.
-# Purchase of a commercial license is mandatory for any use of the
-# ENN-release SDK Software in commercial settings.
-#
-# END COPYRIGHT
+// Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
+// All Rights Reserved.
+// Issued under the Academic Public License.
+
+// You can be released from the terms, and requirements of the Academic Public
+// License by purchasing a commercial license.
+// Purchase of a commercial license is mandatory for any use of the
+// ENN-release SDK Software in commercial settings.
+
+// END COPYRIGHT
 import { useEffect, useState } from "react";
 import { FaGithub, FaBookOpen, FaInfoCircle } from "react-icons/fa";
 import { SiFastapi } from "react-icons/si";
