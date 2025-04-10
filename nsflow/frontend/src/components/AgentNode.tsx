3f1c424c430bc3ff33801d7a3ae70ec7855d93eb
Singh <228496@us-k7qqqjr3cx-cts.attlocal.net>
2025-04-10 14:33:14 -0700

    replace ENN-release with nsflow

--- nsflow/frontend/src/components/AgentNode.tsx
+++ nsflow/frontend/src/components/AgentNode.tsx
@@ -2,12 +2,12 @@
 // Copyright (C) 2023-2025 Cognizant Digital Business, Evolutionary AI.
 // All Rights Reserved.
 // Issued under the Academic Public License.
-
+//
 // You can be released from the terms, and requirements of the Academic Public
 // License by purchasing a commercial license.
 // Purchase of a commercial license is mandatory for any use of the
-// ENN-release SDK Software in commercial settings.
-
+// nsflow SDK Software in commercial settings.
+//
 // END COPYRIGHT
 import React, { useState, useMemo } from "react";
 import { Handle, Position, NodeProps } from "reactflow";
