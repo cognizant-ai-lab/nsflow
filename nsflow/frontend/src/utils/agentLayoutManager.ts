decf3af606a44ea000cdd5265d7efb6293bfa166
deepsaia <deepak.singh10@cognizant.com>
2026-08-27 15:15:06 +0530

    Migrate agentLayoutManager to @xyflow/react

--- nsflow/frontend/src/utils/agentLayoutManager.ts
+++ nsflow/frontend/src/utils/agentLayoutManager.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { Node, Edge } from "reactflow";
+import { Node, Edge } from "@xyflow/react";
 import { hierarchicalRadialLayout } from "./hierarchicalRadialLayout";
 import { getPositionCache } from "./agentPositionCache";
 
