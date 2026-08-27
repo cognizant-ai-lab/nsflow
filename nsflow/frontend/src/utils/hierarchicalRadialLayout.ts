62aeae00b2baa9150424bd4f10a0a4deb2d26126
deepsaia <deepak.singh10@cognizant.com>
2026-08-27 15:15:06 +0530

    Migrate hierarchicalRadialLayout to @xyflow/react

--- nsflow/frontend/src/utils/hierarchicalRadialLayout.ts
+++ nsflow/frontend/src/utils/hierarchicalRadialLayout.ts
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { Node } from "reactflow";
+import { Node } from "@xyflow/react";
 
 export interface ExtendedNode extends Node {
   style?: {
